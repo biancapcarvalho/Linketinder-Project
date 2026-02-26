ad64ee7ebb8c7017ad6ed05f3a1b96dc2fe106db
Bianca Carvalho <caarvalhobianca@gmail.com>
2026-02-26 11:59:15 -0300

    fix: subir controller que faltou no commit inicial

---
+++ src/controller/AppController.groovy
@@ -0,0 +1,41 @@
+package controller
+
+import view.MenuView
+
+class AppController {
+    MenuView view
+    CandidatoController candidatoController
+    EmpresaController empresaController
+
+    void iniciar() {
+        view.exibirIntro()
+
+        boolean executando = true
+        Scanner scanner = new Scanner(System.in)
+
+        while (executando) {
+            view.exibirMenu()
+            Integer input = 0
+            try {
+                input = scanner.nextLine() as Integer
+            } catch (NumberFormatException e) {
+                println "Não consegui entender. Você deve informar um número do menu"
+                continue
+            }
+
+            switch (input) {
+                case 1:
+                    candidatoController.listarCandidatos()
+                    break
+                case 2:
+                    empresaController.listarEmpresas()
+                    break
+                case 3:
+                    executando = false
+                    break
+                default:
+                    println "Não consegui entender. Você deve informar um número do menu"
+            }
+        }
+    }
+}
